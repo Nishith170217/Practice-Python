695643ff2905a6d814d088cb2db03ecfec99acf8
Nishith Ranjan Biswas <nishithranjanbiswas@Nishiths-Air.fritz.box>
2024-01-02 18:54:07 +0100

    initial_
    commit

---
+++ prac1.py
@@ -0,0 +1,7 @@
+def age100():
+    name = input('Enter your Name: ')
+    age = eval(input('Enter your age: ')) 
+    yr = (2024-age)+100
+    print(name +' Your age will be 100 in the year of '+ str(yr))
+
+age100()
