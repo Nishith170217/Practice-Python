dfe351483ddd3da255aab1bde033c81807b2c7e3
Nishith <45823099+Nishith170217@users.noreply.github.com>
2024-01-13 17:01:00 +0100

    Add files via upload

---
+++ pract16.py
@@ -0,0 +1,7 @@
+import random
+def passwordGen():
+    s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+    passlen = 8
+    p =  "".join(random.sample(s,passlen))
+    return p
+print(passwordGen())
