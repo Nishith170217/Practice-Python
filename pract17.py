060b60e5671b56048cd803bc4092df8c6c2729e7
Nishith <45823099+Nishith170217@users.noreply.github.com>
2024-01-14 18:02:43 +0100

    Add files via upload

---
+++ pract17.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+url = 'http://www.nytimes.com'
+r = requests.get(url)
+r_html = r.text
+soup = BeautifulSoup(r_html)
+title = soup.find('span', 'articletitle').string
